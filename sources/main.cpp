620a729bb2e313a4c7b7164e05166a32e36c07d5
Kolpakova Lera <11198lera@mail.ru>
2017-09-28 17:41:41 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,23 +1,38 @@
+//
+//  main.cpp
+//  stack_main.cpp
+//
+//  Created by Валерия Колпакова on 28.09.17.
+//  Copyright © 2017 Валерия Колпакова. All rights reserved.
+//
+
 #include <iostream>
 #include "stack.hpp"
 using namespace std;
 
-int main() 
+int main()
 {
-	cout << "ЭЛЕМЕНТЫ СТЕКА: " << endl;
-		
-	stack<int> intStack;
-	intStack.push(0);
-	intStack.push(1);
-	intStack.push(2);
-	intStack.push(3);
-	intStack.push(4);
-
-	for (int i = 0; i < 4; i++) 
-	{
-		cout << intStack.pop() << endl;
-	}
-		 
-	cout << "Номер элемента стека: " << intStack.count() << endl;
-	return 0;	
+    
+    stack<int> intStack;
+    
+    intStack.push(0);
+    intStack.push(1);
+    intStack.push(2);
+    intStack.push(3);
+    intStack.push(4);
+    intStack.push(5);
+    intStack.push(6);
+    intStack.push(7);
+    intStack.push(8);
+    intStack.push(9);
+    
+    intStack.print();
+    
+    for (int i = 0; i < 10; i++) {
+        cout << intStack.pop() << endl;
+    }
+    
+    //cout << "Номер элемента стека: " << intStack.count() << endl;
+    
+    return 0;
 }
